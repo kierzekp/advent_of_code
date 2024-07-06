aab54dc94a81c1cd52399bf1514c4c71be59fa97
kierzekp <whoatemy@gmail.com>
2024-07-06 21:33:39 +0200

    2015.07 finish part 1

--- 2015/07.rs
+++ 2015/07.rs
@@ -1,6 +1,8 @@
 use std::fs;
+use std::collections::HashSet;
 use std::collections::HashMap;
 
+#[derive(Clone, Copy, PartialEq)]
 struct Wire {
   active: bool,
   value: u16,
@@ -32,7 +34,7 @@ struct Gate {
   in_wire_2: Option<String>,
   in_value: Option<u16>,
   gate_type: GateType,
-  shift_value: Option<u8>,
+  shift_value: Option<u16>,
   out_wire: String,
 }
 
@@ -54,10 +56,7 @@ fn parse_gate(connection_def: Vec<&str>) -> Option<Gate> {
       return Some(Gate {
         in_wire_1: input_wire_name,
         in_wire_2: None,
-        in_value: match input_value {
-          0 => None,
-          _ => Some(input_value),
-        },
+        in_value: Some(input_value),
         gate_type: GateType::PassThrough,
         shift_value: None,
         out_wire: output_wire,
@@ -87,12 +86,27 @@ fn parse_gate(connection_def: Vec<&str>) -> Option<Gate> {
       });
     }
     5 => {
+      let mut input_value: u16 = 0;
+      let mut input_wire_name: Option<String> = None;
+      match connection_def[0].parse::<u16>() {
+        Ok(n) => input_value = n,
+        Err(e) => input_wire_name = Some(String::from(connection_def[0])),
+      }
+      
+      let mut shift_value: u16 = 0;
+      match connection_def[2].parse::<u16>() {
+        Ok(n) => shift_value = n,
+        Err(e) => (),
+      }
       match connection_def[1] {
         "AND" => {
           return Some(Gate {
-            in_wire_1: Some(String::from(connection_def[0])),
+            in_wire_1: input_wire_name,
             in_wire_2: Some(String::from(connection_def[2])),
-            in_value: None,
+            in_value: match input_value {
+              0 => None,
+              _ => Some(input_value),
+            },
             gate_type: GateType::And,
             shift_value: None,
             out_wire: output_wire,
@@ -100,9 +114,12 @@ fn parse_gate(connection_def: Vec<&str>) -> Option<Gate> {
         }
         "OR" => {
           return Some(Gate {
-            in_wire_1: Some(String::from(connection_def[0])),
+            in_wire_1: input_wire_name,
             in_wire_2: Some(String::from(connection_def[2])),
-            in_value: None,
+            in_value: match input_value {
+              0 => None,
+              _ => Some(input_value),
+            },
             gate_type: GateType::Or,
             shift_value: None,
             out_wire: output_wire,
@@ -116,7 +133,7 @@ fn parse_gate(connection_def: Vec<&str>) -> Option<Gate> {
                 in_wire_2: None,
                 in_value: None,
                 gate_type: GateType::LeftShift,
-                shift_value: None,
+                shift_value: Some(shift_value),
                 out_wire: output_wire,
               });
             }
@@ -131,7 +148,7 @@ fn parse_gate(connection_def: Vec<&str>) -> Option<Gate> {
                 in_wire_2: None,
                 in_value: None,
                 gate_type: GateType::RightShift,
-                shift_value: None,
+                shift_value: Some(shift_value),
                 out_wire: output_wire,
               });
             }
@@ -169,13 +186,108 @@ fn print_gate_info(gate: Gate) {
     gate.shift_value.unwrap_or(0), gate.out_wire);
 }
 
-fn initialize_sim(wires: &mut HashMap<String, Wire>, gates: Vec<Gate>) {
+fn iterate_sim(wires: &mut HashMap<String, Wire>, gates: &Vec<Gate>) {
   for gate in gates {
-    if gate.gate_type == GateType::PassThrough && gate.in_wire_1 == None {
-      let wire = wires.get(&gate.out_wire).unwrap();
+    // println!("i1: {}", gate.in_wire_1.as_ref().unwrap_or(&String::new()));
+    let mut input_wire_1_copy: Option<Wire> = None;
+    {
+      input_wire_1_copy = wires.get(&gate.in_wire_1.as_ref().unwrap_or(&String::new()).clone()).copied();
+    }
+    let mut input_wire_2_copy: Option<Wire> = None;
+    {
+      input_wire_2_copy = wires.get(&gate.in_wire_2.as_ref().unwrap_or(&String::new()).clone()).copied();
+    }
+    let out_wire = &mut wires.get_mut(&gate.out_wire).unwrap();
 
+    if gate.gate_type == GateType::PassThrough {
+      match input_wire_1_copy {
+        Some(iw1) => {
+          if iw1.active {
+            out_wire.active = true;
+            out_wire.value = iw1.value;
+          }
+        }
+        None => {
+          out_wire.active = true;
+          out_wire.value = gate.in_value.unwrap();
+        }
+      }
+    }
+    if gate.gate_type == GateType::Not {
+      match input_wire_1_copy {
+        Some(iw1) => {
+          if iw1.active {
+            out_wire.active = true;
+            out_wire.value = !iw1.value;
+          }
+        }
+        None => {
+          out_wire.active = true;
+          out_wire.value = !gate.in_value.unwrap();
+        }
+      }
+    }
+    if gate.gate_type == GateType::LeftShift {
+      let input_wire = input_wire_1_copy.unwrap();
+      if input_wire.active {
+        out_wire.active = true;
+        out_wire.value = input_wire.value << gate.shift_value.unwrap_or(0);
+      }
+    }
+    if gate.gate_type == GateType::RightShift {
+      let input_wire = input_wire_1_copy.unwrap();
+      if input_wire.active {
+        out_wire.active = true;
+        out_wire.value = input_wire.value >> gate.shift_value.unwrap_or(0);
+      }
+    }
+    if gate.gate_type == GateType::Or {
+      let input_wire_2 = input_wire_2_copy.unwrap();
+      match input_wire_1_copy {
+        Some(iw1) => {
+          if iw1.active && input_wire_2.active {
+            out_wire.active = true;
+            out_wire.value = iw1.value | input_wire_2.value;
+          }
+        }
+        None => {
+          if input_wire_2.active {
+            out_wire.active = true;
+            out_wire.value = gate.in_value.unwrap() | input_wire_2.value;
+          }
+        }
+      }
+    }
+    if gate.gate_type == GateType::And {
+      let input_wire_2 = input_wire_2_copy.unwrap();
+      match input_wire_1_copy {
+        Some(iw1) => {
+          if iw1.active && input_wire_2.active {
+            out_wire.active = true;
+            out_wire.value = iw1.value & input_wire_2.value;
+          }
+        }
+        None => {
+          if input_wire_2.active {
+            out_wire.active = true;
+            out_wire.value = gate.in_value.unwrap() & input_wire_2.value;
+          }
+        }
+      }
     }
   }
+}
+
+fn get_active_wires(wires: &HashMap<String, Wire>) -> HashSet<String> {
+  let mut active_wires = HashSet::new();
+
+  for wire_name in wires.keys() {
+    if wires.get(wire_name).unwrap().active {
+      active_wires.insert(String::from(wire_name));
+    }
+  }
+
+  return active_wires;
 }
 
 fn main() {
@@ -186,6 +298,14 @@ fn main() {
   let mut gates: Vec<Gate> = Vec::new();
 
   parse_connection_definitions(&mut wires, &mut gates, file_contents.split("\n").collect());
+  
+  let mut iteration = 0;
+  let mut a_copy = wires.get("a").copied();
+  while !a_copy.unwrap().active {
+    iterate_sim(&mut wires, &gates);
+    a_copy = wires.get("a").copied();
+    iteration += 1;
+  }
 
-  initialize_sim(&mut wires, gates);
+  println!("Value at wire a at the end of the simulation: {}", a_copy.unwrap().value);
 }
